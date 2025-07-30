4d1d6a14a353685eb99dd173be9bba53ee9b4fde
Uri <urigat@gmail.com>
2025-07-30 09:37:35 +0300

    remove old

--- new-react-component-template/{{name}}/{{name}}.tsx
+++
@@ -1,8 +0,0 @@
-import "./NewComponent.css";
-import * as React from "react";
-
-export default class {{name}} extends React.Component<any, any> {
-  public render() {
-    return <div className="{{name}}">{{name}}</div>;
-  }
-}
